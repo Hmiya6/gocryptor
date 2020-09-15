f83b04310ab0126c1429ae96c9340ae6deffff04
Hmiya <h.miya.work@gmail.com>
2020-09-15 19:51:12 +0900

    small update

--- utils.go
+++ utils.go
@@ -8,7 +8,13 @@ import (
 func enumFiles(root string) ([]string, error) {
 	var files []string
 	err := filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
+		// pass directory
 		if info.IsDir() {
+			return nil
+		}
+		// pass itself
+		// [2:] strips "./" in "./yourExecutiveFile"
+		if info.Name() == os.Args[0][2:] {
 			return nil
 		}
 		files = append(files, path)
